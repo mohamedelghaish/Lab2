e19789ad8a168439f63c89798f7cb0812ea1552d
Ahmed Abufoda <ahmed.abufoda70@gmail.com>
2023-12-27 13:48:58 +0200

    add function

--- HelloITI.java
+++ HelloITI.java
@@ -1,9 +1,9 @@
 class HelloWorld{
 		public static void main (String [] args){
-		        printHelloGit();
+		        printHello();
 			
 		}
-		public void printHelloGit(){
-			System.out.println("Hello Git" );
+		public void printHello(){
+			System.out.println("Hello " );
 		}
 }
